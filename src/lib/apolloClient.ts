40b19a8363f6367233ab74d1c9fb12bf62888332
maaaashi <doyaseizin.7@gmail.com>
2023-10-13 00:56:11 +0900

    set apollo client

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,18 +1,14 @@
+'use client'
+
 import { createAuthLink, AuthOptions } from 'aws-appsync-auth-link'
 import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link'
 import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client/core'
 
-const {
-  NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-  NEXT_PUBLIC_GRAPHQL_REGION,
-  NEXT_PUBLIC_GRAPHQL_APIKEY,
-} = process.env
-
-const url = NEXT_PUBLIC_GRAPHQL_ENDPOINT
-const region = NEXT_PUBLIC_GRAPHQL_REGION
+const url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
+const region = process.env.NEXT_PUBLIC_GRAPHQL_REGION
 const auth: AuthOptions = {
   type: 'API_KEY',
-  apiKey: NEXT_PUBLIC_GRAPHQL_APIKEY,
+  apiKey: process.env.NEXT_PUBLIC_GRAPHQL_APIKEY,
 }
 
 const link = ApolloLink.from([
