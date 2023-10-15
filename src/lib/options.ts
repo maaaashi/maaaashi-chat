6431d52a69fb6cc54a2d10cd6064b304549a9d80
maaaashi <doyaseizin.7@gmail.com>
2023-10-15 17:05:31 +0900

    update nextauth options

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -1,39 +1,7 @@
-import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import { NextAuthOptions } from 'next-auth'
-// import { DynamoDBAdapter } from '@auth/dynamodb-adapter'
 import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import CognitoProvider from 'next-auth/providers/cognito'
 
-const {
-  NEXT_AUTH_AWS_ACCESS_KEY: accessKeyId,
-  NEXT_AUTH_AWS_SECRET_KEY: secretAccessKey,
-  NEXT_AUTH_AWS_REGION: region,
-  GITHUB_ID,
-  GITHUB_SECRET,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_SECRET,
-  NODE_ENV,
-  COGNITO_CLIENT_ID,
-  COGNITO_ISSUER,
-} = process.env
-
-const config: DynamoDBClientConfig = {
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  },
-  region,
-}
-
-const client = DynamoDBDocument.from(new DynamoDB(config), {
-  marshallOptions: {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  },
-})
+const { GITHUB_ID, GITHUB_SECRET, NODE_ENV } = process.env
 
 export const options: NextAuthOptions = {
   debug: NODE_ENV === 'development',
@@ -45,20 +13,5 @@ export const options: NextAuthOptions = {
       clientId: GITHUB_ID ?? '',
       clientSecret: GITHUB_SECRET ?? '',
     }),
-    // GoogleProvider({
-    //   clientId: GOOGLE_CLIENT_ID ?? '',
-    //   clientSecret: GOOGLE_SECRET ?? '',
-    // }),
-    // CognitoProvider({
-    //   clientId: COGNITO_CLIENT_ID ?? '',
-    //   clientSecret: '',
-    //   issuer: COGNITO_ISSUER ?? '',
-    //   client: {
-    //     token_endpoint_auth_method: 'none',
-    //   },
-    // }),
   ],
-  // adapter: DynamoDBAdapter(client, {
-  //   tableName: 'chat-app',
-  // }),
 }
