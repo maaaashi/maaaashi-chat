03fc453ed20d7a26bd87996012f30982cbb17e7a
maaaashi <doyaseizin.7@gmail.com>
2023-10-16 12:35:37 +0900

    design調整

--- src/components/ChatArea/index.tsx
+++ src/components/ChatArea/index.tsx
@@ -71,7 +71,7 @@ export const ChatArea = () => {
 
   return (
     <div className='h-full flex flex-col'>
-      <div className='flex-1'>
+      <div className='flex-1 overflow-y-auto p-3'>
         {chats.map((c, index) => (
           <ChatBubble chat={c} key={index} />
         ))}
